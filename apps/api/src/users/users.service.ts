bac4beb7aecdb05faac64548d3a4f67d1439f205
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-04 09:22:42 +0530

    fix: throw exceptiton on unique contraint hit

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUser } from './interfaces/create-user.interface';
@@ -9,6 +9,10 @@ export class UsersService {
   constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {}
 
   async create(data: CreateUser) {
+    if (await this.userModel.exists({ email: data.email })) {
+      throw new ConflictException('User already exists!');
+    }
+
     return this.userModel.create(data);
   }
 }
