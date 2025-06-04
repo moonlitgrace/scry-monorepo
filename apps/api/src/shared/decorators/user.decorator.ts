39c14f27242ef5d755a56131716f927076c7ada2
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-04 18:55:30 +0530

    refactor: local auth strategy with minimal payload (#98)

--- apps/api/src/shared/decorators/user.decorator.ts
+++ apps/api/src/shared/decorators/user.decorator.ts
@@ -1,7 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { Request } from 'express';
+import { AccessTokenPayload } from 'src/auth/interfaces/access-token';
 
-export const User = createParamDecorator((_, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest<Request>();
-  return request.user;
-});
+export type AuthenticatedUser = AccessTokenPayload;
+
+export const User = createParamDecorator(
+  (_, ctx: ExecutionContext): AuthenticatedUser => {
+    const request = ctx.switchToHttp().getRequest<Request>();
+    return request.user as AuthenticatedUser;
+  },
+);
