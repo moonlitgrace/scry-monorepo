b400c06c8197e688583f73da23d4803e10905364
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-04 20:45:48 +0530

    chore: move decorators inside auth app

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Public } from 'src/shared/decorators/public.decorator';
-import { AuthenticatedUser, User } from 'src/shared/decorators/user.decorator';
 import { AuthService } from './auth.service';
+import { Public } from './decorators/public.decorator';
+import { AuthenticatedUser, User } from './decorators/user.decorator';
 
 @Public()
 @Controller('auth')
