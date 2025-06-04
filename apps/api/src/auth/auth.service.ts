ccf45da475eaab1c26c378d27235b794bd770f84
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-04 09:38:42 +0530

    refactor: use bcrypt to hash password before store

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { SignInDto } from './dto/sign-in.dto';
+import { hashPass } from './utils';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
 
-  signIn(data: SignInDto) {
-    return this.usersService.create(data);
+  async signIn(data: SignInDto) {
+    const hashedPassword = await hashPass(data.password);
+    return this.usersService.create({ email: data.email, password: hashedPassword });
   }
 }
