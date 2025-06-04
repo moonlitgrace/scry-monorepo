6ef830fc53cd0f4a6981ec727f2f0fd8b5aab1ee
MO0nlitgrace <moonlitgrace.gaia@gmail.com>
2025-06-04 21:43:39 +0530

    chore: rename jwt guard with prefix

--- apps/api/src/auth/guards/jwt.guard.ts
+++ apps/api/src/auth/guards/jwt.guard.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {
+export class JwtAuthGuard extends AuthGuard('jwt') {
   constructor(private readonly reflector: Reflector) {
     super();
   }
